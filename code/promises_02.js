d18e142045c52e0ab0628d1477d19a6c60f2cf54
Luis Alfonso Talegón Puente <luisetex@gmail.com>
2020-02-21 08:39:38 +0100

    turn promisifiedTimeOut into a function

--- code/promises_02.js
+++ code/promises_02.js
@@ -1,15 +1,21 @@
-const promisifiedTimeOut = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Hello!');
-  }, 3000);
-});
+const promisifiedTimeOut = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello!');
+    }, 3000);
+  });
+}
 
-promisifiedTimeOut
+promisifiedTimeOut()
   .then((result) => {
     console.log(result);
     return result.toUpperCase();
   })
   .then((result) => {
     console.log(result)
+  })
+  .then(() => {
+    promisifiedTimeOut()
+    .then(console.log)
   })
   .catch((error) => console.log(error))
